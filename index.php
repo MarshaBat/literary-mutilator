d950ed6e4d1e43eabcd8777cb72981e6bf3b9d41
Marsha Battee <marsha@sanesuite.com>
2019-08-08 16:24:56 -0600

    added additional button, styling to form and sections descriptors

--- index.php
+++ index.php
@@ -10,11 +10,19 @@
 
 	</head>
 	<body>
+		<h2>Literary Mutilator - Javascript Challenge - Marsha Battee</h2>
+		<div>
+			<h3>This HTML Form <mark>highlights</mark>:</h3>
+				<ul>
+					<li>2 options from Tier I (changing font and text color)</li>
+					<li>1 option from Tier II (highlighting 1 word)</li>
+				</ul>
+		</div>
 
 		<div>
 			<form action="#" method="post">
 				<div>
-					<label for="name">Name:</label>
+					<label for="name" id="color">Name:</label>
 					<input type="text" id="name" name="user_name">
 				</div>
 				<div>
@@ -22,14 +30,26 @@
 					<input type="email" id="mail" name="user_mail">
 				</div>
 				<div>
-					<label for="msg">Message:</label>
+					<label for="msg" id="font">Message:</label>
 					<textarea id="msg" name="user_message"></textarea>
 				</div>
 				<div class="button">
 					<button type="submit">Send your message</button>
+					<button type="button" onclick="changeFont()">Change font of "message"</button>
+					<button type="button" onclick="changeColor()">Change color of "name"</button>
 				</div>
 			</form>
+		</div>
 
+		<div>
+			<h3>This section highlights: </h3>
+			<ul>
+				<li>1 option from Tier III (drag and drop image)</li>
+			</ul>
+			<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
+				<img src="headshot.jpg" draggable="true" ondragstart="drag(event)" id="drag1" width="200" height="200" alt="photo">
+			</div>
+			<div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
 		</div>
 
 
@@ -63,13 +83,7 @@
 				aliquet pulvinar sapien, nec tempus nunc egestas quis. Quisque finibus at lectus sit amet gravida.</p>
 		</section>
 
-		<div>
-		<h3>This is a drap and drop pic.</h3>
-		<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
-			<img src="headshot.jpg" draggable="true" ondragstart="drag(event)" id="drag1" width="200" height="200">
-		</div>
-		<div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-		</div>
+
 
 
 
--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-//window.onload = function() {
-
+//This changes the color of the "name" text by clicking on the button in the form
 function changeColor() {
 	document.getElementById("color").style.color = "magenta";
 }
 
+//This changes the font of the "message" text by clicking on the button in the form
 function changeFont() {
 	document.getElementById("font").style.fontFamily = "Arial Black";
-
 }
 
 var x = 0;
@@ -14,7 +13,7 @@ function scroll() {
 	document.getElementById("box").innerHTML = x += 1;
 }
 
-
+//This is the drag and drop image section
 function allowDrop(ev) {
 	ev.preventDefault();
 }
@@ -28,6 +27,3 @@ function drop(ev) {
 	var data = ev.dataTransfer.getData("text");
 	ev.target.appendChild(document.getElementById(data));
 }
-
-
-//};
