e8e6e54fb4ad87b8a95b6fbc2bc7ed90aba33c35
Marsha Battee <marsha@sanesuite.com>
2019-08-08 13:30:54 -0600

    changed font and color, added buttons to p1 and p2

--- index.php
+++ index.php
@@ -3,24 +3,35 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Literary-Mutilator (Javascript Challenge)</title>
+		<script src="script.js"></script>
+
+		<!-- Assignment at this link: https://bootcamp-coders.cnm.edu/class-materials/javascript/events/assignment.php -->
 
 	</head>
 	<body>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sapien condimentum, condimentum magna at,
+		<h2>Lorem Ipsum</h2>
+		<p class="p1" id="color">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sapien condimentum, condimentum magna at,
 			aliquam dolor. Cras consectetur nec sem quis pulvinar. Etiam bibendum dapibus lectus, accumsan dignissim arcu
 			aliquet ac. In hac habitasse platea dictumst. Sed scelerisque magna orci, eget suscipit nunc bibendum ut.
 			Vivamus eu eros fringilla, suscipit lorem at, venenatis sapien. Fusce at volutpat magna. Curabitur elementum
 			nisi posuere lectus pretium fringilla. Nam viverra ultricies tempus. Cras vestibulum malesuada lacus, in
 			ullamcorper quam dictum at.</p>
-		<p>Fusce luctus scelerisque nulla in gravida. Pellentesque eget eros at nibh mattis faucibus. Nulla facilisi.
+		<button type="button" onclick="changeColor()">Set text color</button>
+
+		<p class="p2" id="font">Fusce luctus scelerisque nulla in gravida. Pellentesque eget eros at nibh mattis faucibus. Nulla facilisi.
 			Vivamus auctor pretium nisi ac lacinia. In hac habitasse platea dictumst. Quisque vitae mattis tortor. Morbi in
 			ullamcorper ante. Aenean tincidunt congue mattis. Nam a luctus mi, in placerat dolor. Maecenas lacinia est eu
 			est mollis convallis. Mauris mi risus, tincidunt eget odio eu, sagittis luctus est.</p>
-		<p>Vivamus luctus nisi consequat leo gravida, in vehicula libero feugiat. Duis arcu ligula, convallis vitae ex
+		<button type="button" onclick="changeFont()">Set font</button>
+
+
+		<p class="p3">Vivamus luctus nisi consequat leo gravida, in vehicula libero feugiat. Duis arcu ligula, convallis vitae ex
 			sed, pulvinar accumsan sapien. Etiam facilisis diam nec nulla dapibus sollicitudin. Vestibulum at libero eu
 			nisl aliquam lacinia. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
 			Maecenas a tincidunt augue. Pellentesque euismod consequat orci et lobortis. Sed aliquet pulvinar sapien, nec
 			tempus nunc egestas quis. Quisque finibus at lectus sit amet gravida.</p>
+
+
 
 
 	</body>
---
+++ script.js
@@ -0,0 +1,13 @@
+//window.onload = function() {
+
+function changeColor() {
+	document.getElementById("color").style.color = "magenta";
+}
+
+function changeFont() {
+	document.getElementById("font").style.fontFamily = "Arial Black";
+
+}
+
+
+//};
